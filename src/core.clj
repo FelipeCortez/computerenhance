1b9a887e5816f5b55ba31bf84c05ef1e2797c394
Felipe Cortez <github@felipecortez.net>
2023-09-24 11:27:52 -0300

    Simplify

--- src/core.clj
+++ src/core.clj
@@ -32,61 +32,55 @@
 
 (defmacro locals [& xs] (zipmap (map keyword xs) xs))
 
-(defn decode [instruction]
-  (let [byte-1
-        (bit-shift-right instruction 8)
+(defn decode [byte byte-stream]
+  (let [byte-1 byte, byte-2 (.read byte-stream)]
+    (cond
+      ;; register/memory to/from register
+      (= (bit-and byte-1 2r11111100) 2r10001000)
+      (let [d?
+            (not (zero? (bit-and byte-1 2r00000010)))
 
-        byte-2
-        (bit-and instruction 2r11111111)
+            w?
+            (not (zero? (bit-and byte-1 2r00000001)))
 
-        opcode
-        (case (bit-and byte-1 2r11111100)
-          2r10001000 :reg-reg)
+            _mod
+            (-> byte-2
+                (bit-and 2r11000000)
+                (bit-shift-right 6))
 
-        d
-        (bit-and byte-1 2r00000010)
+            reg
+            (-> byte-2
+                (bit-and 2r00111000)
+                (bit-shift-right 3))
 
-        w
-        (bit-and byte-1 2r00000001)
+            rm
+            (bit-and byte-2 2r00000111)
 
-        mod
-        (-> byte-2
-            (bit-and 2r11000000)
-            (bit-shift-right 6))
+            ->reg (if w? w1 w0)
 
-        reg
-        (-> byte-2
-            (bit-and 2r00111000)
-            (bit-shift-right 3))
-
-        rm
-        (bit-and byte-2 2r00000111)
-
-        ->reg (if (zero? w) w0 w1)
-
-        [src dst]
-        (map ->reg
-             (if (zero? d) [reg rm] [rm reg]))]
-    (locals opcode d w mod reg rm src dst)))
-
-(defn read-instructions [f]
-  (with-open [r (io/input-stream f)]
-    (loop [instructions []]
-      (let [b (.read r)]
-        (if (= b -1)
-          instructions
-          (recur (conj instructions (bit-or (bit-shift-left b 8)
-                                            (.read r)))))))))
-
+            [src dst]
+            (map ->reg (if d? [rm reg] [reg rm]))]
+        (format "mov %s, %s" (name dst) (name src))))))
 
 (defn decode-file [f]
-  (map (comp (fn [{:keys [src dst]}] (format "mov %s, %s"
-                                             (name dst)
-                                             (name src)))
-             decode)
-       (read-instructions f)))
+  (with-open [byte-stream (io/input-stream f)]
+    (loop [instructions []]
+      (let [byte (.read byte-stream)]
+        (if (= byte -1)
+          instructions
+          (recur (conj instructions (decode byte byte-stream))))))))
 
 (comment
   (decode-file "support/one-instruction")
   (decode-file "support/multiple-instructions")
+
+  #_#_(= (bit-and byte-1 2r11110000) 2r10110000)
+  (let [w?
+        (zero? (bit-and byte-1 2r00000001))
+
+        reg
+        (bit-and byte-2 2r00000111)
+
+        ]
+    #_(locals src dst))
   nil)
